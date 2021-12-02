4090430d605f80b2dd0b4965ce9962d8211db193
Ken <kenkong03014@gmail.com>
2021-12-02 14:47:08 -0800

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,22 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TextcomponentComponent } from './textcomponent/textcomponent.component';
-import { ImagecomponentComponent } from './imagecomponent/imagecomponent.component';
-import { EmpInfoComponent } from './emp-info/emp-info.component';
-import { FormComponentComponent } from './form-component/form-component.component';
-import { EInfoComponent } from './e-info/e-info.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { IntroComponent } from './Components/intro/intro.component';
+import { CoursesComponent } from './Components/courses/courses.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { LoginComponent } from './Components/login/login.component';
+import { HomeComponent } from './Components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TextcomponentComponent,
-    ImagecomponentComponent,
-    EmpInfoComponent,
-    FormComponentComponent,
-    EInfoComponent,
+    NavbarComponent,
+    IntroComponent,
+    CoursesComponent,
+    FooterComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
